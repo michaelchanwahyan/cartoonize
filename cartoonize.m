12290ae2724fe255b1b17bd6f9c17d4b9aaee95b
michaelchan <michaelchan>
2022-04-26 22:12:52 +0800

    [update]

--- cartoonize.m
+++ cartoonize.m
@@ -1,23 +1,33 @@
-function img_q = imgq(img, intvlNum)
-
-[M,N,~] = size(img); MN = M*N;
+function img_q = cartoonize(img, intvlNum, intvlRank)
+% intvlRank either be 'min', 'max', or 'mean'
 
 img_q = img;
-img1 = img(:,:,1); img1_s = sort(img1(:));
-img2 = img(:,:,2); img2_s = sort(img2(:));
-img3 = img(:,:,3); img3_s = sort(img3(:));
-bnd = 1 : MN / (intvlNum+1) : MN;
-for bndIdx = 1:intvlNum
-    lowbnd = bnd(bndIdx); uppbnd = bnd(bndIdx+1);
-    bool_idx = img1 > img1_s(lowbnd) & img1 <= img1_s(uppbnd);
-    img1(bool_idx) = mean(img1(bool_idx));
-    bool_idx = img2 > img2_s(lowbnd) & img2 <= img2_s(uppbnd);
-    img2(bool_idx) = mean(img2(bool_idx));
-    bool_idx = img3 > img3_s(lowbnd) & img3 <= img3_s(uppbnd);
-    img3(bool_idx) = mean(img3(bool_idx));
+
+img1 = img(:,:,1);
+img2 = img(:,:,2);
+img3 = img(:,:,3);
+bnd = 1 : max(img(:)) / intvlNum : max(img(:));
+for bndIdx = 1:max(intvlNum-1, 1)
+    lowbnd = round(bnd(bndIdx)); uppbnd = round(bnd(bndIdx+1));
+    bool_idx = img1 > lowbnd & img1 <= uppbnd;
+    img1(bool_idx) = intvlFunc([lowbnd, uppbnd], intvlRank);
+    bool_idx = img2 > lowbnd & img2 <= uppbnd;
+    img2(bool_idx) = intvlFunc([lowbnd, uppbnd], intvlRank);
+    bool_idx = img3 > lowbnd & img3 <= uppbnd;
+    img3(bool_idx) = intvlFunc([lowbnd, uppbnd], intvlRank);
 end
 img_q(:,:,1) = img1;
 img_q(:,:,2) = img2;
 img_q(:,:,3) = img3;
 
+end
+
+function returnVal = intvlFunc(signals, intvlRank)
+if strcmp(intvlRank, 'min')
+    returnVal = min(signals(:));
+elseif strcmp(intvlRank, 'max')
+    returnVal = max(signals(:));
+elseif strcmp(intvlRank, 'mean')
+    returnVal = mean(signals(:));
+end
 end
--- main.m
+++ main.m
@@ -3,13 +3,13 @@ clear
 clc
 
 img = imread('input.jpg');
-img_cartoon = cartoonize(img, 1);
+img_cartoon = cartoonize(img, 2, 'min');
 
 img_f = imfilter(img,fspecial('gaussian', 30,5));
-img_f_cartoon = cartoonize(img_f, 1);
+img_f_cartoon = cartoonize(img_f, 2, 'min');
 
 figure;
 subplot(1,2,1); imshow(img_cartoon);
 subplot(1,2,2); imshow(img_f_cartoon);
 
-imwrite(img_f_cartoon, 'output.jpg');
+%imwrite(img_f_cartoon, 'output.jpg');
